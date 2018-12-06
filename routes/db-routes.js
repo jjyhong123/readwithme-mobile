07300367b9f1ad1beb540e6ef88ec880599cb2f8
Jae <jjyhong123@gmail.com>
2018-12-05 20:43:01 -0800

    increased limits on router

--- routes/db-routes.js
+++ routes/db-routes.js
@@ -2,6 +2,9 @@ const db = require("../models");
 
 const router = require('express').Router();
 
+router.use(bodyParser.json({limit: '10mb', extended: true}))
+router.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
+
 // Create all our routes and set up logic within those routes where required.
 router.post("/save", (req, res) => {
     db.Item.create({ pollyUrl: req.body.pollyUrl, imageUrl: req.body.imageUrl, text: req.body.text })
