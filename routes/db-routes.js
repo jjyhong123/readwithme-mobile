7a0783940a0cb5a6dd6da12bae4a7589770c0327
Jae <jjyhong123@gmail.com>
2018-12-05 22:23:45 -0800

    increased bodyparser limit

--- routes/db-routes.js
+++ routes/db-routes.js
@@ -1,9 +1,13 @@
 const db = require("../models");
+var bodyParser = require("body-parser");
 const router = require('express').Router();
+router.use(bodyParser.json({limit: '500mb'}));
+router.use(bodyParser.urlencoded({limit: '500mb', extended: true}));
 
 // Create all our routes and set up logic within those routes where required.
 router.post("/save", (req, res) => {
     let imageBuffer = Buffer.from(req.body.imageUri, 'base64')
+    
     db.Item.create({ pollyUrl: req.body.pollyUrl, image: imageBuffer, text: req.body.text })
     .then((dbItem) => {
         return db.User.findOneAndUpdate({ googleId: req.user.googleId }, { $push: { library: dbItem._id } }, { new: true })
@@ -12,6 +16,7 @@ router.post("/save", (req, res) => {
         res.render("picture", { user: req.user, src: req.body.pollyUrl, image: req.body.imageUri, text: req.body.text, dbRedirect: true });
     })
     .catch(err => res.json(err))
+    
 });
 
 router.get("/delete/:id", (req, res) => {
